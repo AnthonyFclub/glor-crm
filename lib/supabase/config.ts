25461bf4592e8dcb802d806e14a029a1ced865fb
Anthony Morales <anthonymorales@MacBook-Air-de-Anthony-2.local>
2025-12-30 16:39:45 -0500

    Fix: Update correct Supabase API key

--- lib/supabase/config.ts
+++ lib/supabase/config.ts
@@ -3,6 +3,6 @@ import { createBrowserClient } from '@supabase/ssr'
 export const createClient = () => {
     return createBrowserClient(
         'https://zrjznebxxtlhykyaizaf.supabase.co',
-        'sb_publishable_trplQLyMWTwlTIKykYzgNA_ubB1y0hY'
+        'sb_publishable_trpIQLyMWTwITIKykYzgNA_ubB1y0hY'
     )
 }
